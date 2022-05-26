480f117de82a0953e619cd28b20f9cd6897dc051
hajkgev <104987256+hajkgev@users.noreply.github.com>
2022-05-26 21:53:32 +0400

    Update http.cpp

--- http-server/lib/http.cpp
+++ http-server/lib/http.cpp
@@ -1,1 +1,15 @@
+#include "http.h"
 
+http::Text http::parse(const std::string &str) {
+  http::Text text;
+  text.push_back(http::Line());
+  
+  return text;
+}
+
+void http::Server::getHead(int clientSocket, std::string &head,
+                           std::string &body) {
+ 
+}
+
+int http::Server::parseRequestHead(http::Request &req, const std::string &head) {
