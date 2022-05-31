f4e10e1cacae51cf76e58935ab20e4801107766c
hajkgev <104987256+hajkgev@users.noreply.github.com>
2022-05-31 23:32:13 +0400

    Update scheduler_lib.cpp

--- http-server/scheduler_lib/scheduler_lib.cpp
+++ http-server/scheduler_lib/scheduler_lib.cpp
@@ -1,1 +1,19 @@
+#include "scheduler_lib.h"
 
+void parallel_scheduler::error_check(int out) {
+  if (out != 0) {
+    std::cerr << "ERROR : " << out << std::endl;
+    exit(out);
+  }
+}
+
+
+void *parallel_scheduler::consumer(void *arg) {
+  
+  auto *data = (parallel_scheduler *) arg;
+
+  
+  std::function<void(void *)> func;
+
+  
+  void *func_arg;
