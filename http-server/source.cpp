18afcdbffa6e475cd34c145d109f978a13263eb8
hajkgev <104987256+hajkgev@users.noreply.github.com>
2022-05-31 23:27:26 +0400

    Update source.cpp

--- http-server/source.cpp
+++ http-server/source.cpp
@@ -3,11 +3,17 @@
 struct StaticFile : http::Service {
   std::string content;
 
-  explicit StaticFile(std::string _content)
-      : fileContent(std::move(_content)) {}
+  explicit StaticFile(std::string _fileContent)
+      : fileContent(std::move(_fileContent)) {}
 
-  http::Response doService(const http::Request &req) final {
-    
+  http::Response doService(const http::Request &request) final {
+    http::Response res;
+    res.version = "HTTP/1.0";
+    res.headers["Content-Length"] = std::to_string(fileContent.size());
+    res.body = fileContent;
+    res.statusNumber = std::to_string(200);
+    res.statusInfo = "OK";
+    return res;
   }
 };
 
