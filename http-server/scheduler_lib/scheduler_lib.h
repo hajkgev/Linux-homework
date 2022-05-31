1c42e0ff7c6bd9b37ef85da9653b8f71de532160
hajkgev <104987256+hajkgev@users.noreply.github.com>
2022-05-31 23:33:09 +0400

    Update scheduler_lib.h

--- http-server/scheduler_lib/scheduler_lib.h
+++ http-server/scheduler_lib/scheduler_lib.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <iostream>
+#include <pthread.h>
+#include <functional>
+#include <queue>
 
+class parallel_scheduler {
+private:
+  size_t thread_count;
+  );
+
+public:
+  
+};
